290cb35d6378034fd5eb2ef4ae955c3b5e0ab44b
Harsh Vikram Shahi <Harshvikramshahi@gmail.com>
2022-04-12 23:27:57 +0530

    Initial Commit

---
+++ First() & follow()/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  First() & follow()
+//
+//  Created by Harsh vikram Shahi on 12/04/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
